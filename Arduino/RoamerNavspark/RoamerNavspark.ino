3b2c806f7e2e78577ee200b22ebcec24caae596d
Chris <oodachris@hotmail.co.uk>
2022-04-16 13:28:30 +0100

    Update RoamerNavspark.ino
    basic comms structure working

--- Arduino/RoamerNavspark/RoamerNavspark.ino
+++ Arduino/RoamerNavspark/RoamerNavspark.ino
@@ -1,6 +1,8 @@
 //https://www.navsparkforum.com.tw/viewtopic.php?f=23&t=159&p=542&hilit=uart#p542
 //U08	gnss_uart_rx_receive( U08 uart );
 
+//comms from https://forum.arduino.cc/t/serial-input-basics-updated/382007/3
+
 /* upload procedure if it's borked:
 Plug in USB
 Link BOOT_SEL to RF_GND
@@ -11,6 +13,23 @@ reset
 */
 
 uint8_t led = 13;
+
+const byte numChars = 32;
+char receivedChars[numChars];
+char tempChars[numChars];        // temporary array for use when parsing
+
+// variables to hold the parsed data
+char messageFromPC[numChars] = {0};
+int integerFromPC = 0;
+float floatFromPC = 0.0;
+
+int LEDstartaddr = 0;
+int LEDlength = 0;
+int LEDred = 0;
+int LEDgreen = 0;
+int LEDblue = 0;
+
+boolean newData = false;
 
 
 void setup() {
@@ -24,8 +43,6 @@ void setup() {
   Serial.begin(115200);
 }
 
-
-
 void loop()
 {
   // put your main code here, to run repeatedly:
@@ -33,27 +50,138 @@ void loop()
   int len;
   String str;
   len = sprintf(buf, "$SONAR,360,100*FF\r\n");
-  gnss_uart_putline(0, (U08*) buf, len);
+  //gnss_uart_putline(0, (U08*) buf, len);
 
-  if (Serial.available() > 0)
-  {
-    while (Serial.available() > 0) {
-      int red = Serial.parseInt();
-      int green = Serial.parseInt();
-      int blue = Serial.parseInt();
-      if (Serial.read() == '\n') {
-        //act on it
-        len = sprintf(buf, "R:%03d G:%03d B:%03d\r\n", red, green, blue);
-        gnss_uart_putline(0, (U08*) buf, len);
-      }
-    }
-    str = Serial.readStringUntil('\n');
-          const char* string1 = str.c_str();
-          len = sprintf(buf, string1);
-          gnss_uart_putline(0, (U08*) buf, len);
+
+  recvWithStartEndMarkers();
+  if (newData == true) {
+    strcpy(tempChars, receivedChars);
+    // this temporary copy is necessary to protect the original data
+    //   because strtok() used in parseData() replaces the commas with \0
+    parseLED();
+    showLEDData();
+    newData = false;
   }
 
-  delay(600);
+
+}
+
+void nvsprkSerialPrint(char msgToSend[]) {
+    char buf[32];
+    int len;
+    len = sprintf(buf, "%s", msgToSend);
+    gnss_uart_putline(0, (U08*) buf, len);
+}
+void nvsprkSerialPrintln(char msgToSend[]) {
+    char buf[32];
+    int len;
+    len = sprintf(buf, "%s\r\n", msgToSend);
+    gnss_uart_putline(0, (U08*) buf, len);
+}
+
+
+void recvWithStartEndMarkers() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
+
+  while (Serial.available() > 0 && newData == false) {
+    rc = Serial.read();
+
+    if (recvInProgress == true) {
+      if (rc != endMarker) {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
+    }
+
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
+}
+
+//============
+
+void parseData() {      // split the data into its parts
+
+  char * strtokIndx; // this is used by strtok() as an index
+
+  strtokIndx = strtok(tempChars, ",");     // get the first part - the string
+  strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
+
+  strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+  integerFromPC = atoi(strtokIndx);     // convert this part to an integer
+
+  strtokIndx = strtok(NULL, ",");
+  floatFromPC = atof(strtokIndx);     // convert this part to a float
+
+}
+
+void parseLED() {      // split the data into its parts
+
+  char * strtokIndx; // this is used by strtok() as an index
+
+  strtokIndx = strtok(tempChars, ",");     // get the first part - the string
+  LEDstartaddr = atoi(strtokIndx);  // copy it
+  
+  strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+  LEDlength = atoi(strtokIndx);     // convert this part to an integer
+  
+  strtokIndx = strtok(NULL, ",");
+  LEDred = atoi(strtokIndx);     // convert this part to a float
+  
+  strtokIndx = strtok(NULL, ",");
+  LEDgreen = atoi(strtokIndx);     // convert this part to a float
+  
+  strtokIndx = strtok(NULL, ",");
+  LEDblue = atoi(strtokIndx);     // convert this part to a float
+
+}
+
+//============
+
+void showParsedData() {
+  
+  char buf[32];
+  
+  nvsprkSerialPrint("Message ");
+  nvsprkSerialPrintln(messageFromPC);
+  nvsprkSerialPrint("Integer ");
+  sprintf(buf, "%d", integerFromPC);
+  nvsprkSerialPrintln(buf);
+  nvsprkSerialPrint("Float ");
+  sprintf(buf, "%f", floatFromPC);
+  nvsprkSerialPrintln(buf);
+}
+
+void showLEDData() {
+  
+  char buf[32];
+  
+  nvsprkSerialPrint("LEDstartaddr  ");
+  sprintf(buf, "%d", LEDstartaddr);
+  nvsprkSerialPrintln(buf);
+  
+  nvsprkSerialPrint("LEDlength ");
+  sprintf(buf, "%d", LEDlength);
+  nvsprkSerialPrintln(buf);
+  
+  nvsprkSerialPrint("RGB Values ");
+  sprintf(buf, "R%d, G%d, B%d", LEDred ,LEDgreen , LEDblue);
+  nvsprkSerialPrintln(buf);
+
 }
 
 
@@ -65,15 +193,15 @@ void task_called_after_GNSS_update(void)
 
   if (val == HIGH) {
     digitalWrite(led, HIGH);
-    len = sprintf(buf, "Hello world (GPIO%02d output 1)\r\n", led);
-    gnss_uart_putline(0, (U08*) buf, len);
+    //len = sprintf(buf, "Hello world (GPIO%02d output 1)\r\n", led);
+    //gnss_uart_putline(0, (U08*) buf, len);
     val = LOW;
   }
 
   else if (val == LOW) {
     digitalWrite(led, LOW);
-    len = sprintf(buf, "Hello world (GPIO%02d output 0)\r\n", led);
-    gnss_uart_putline(0, (U08*) buf, len);
+    //len = sprintf(buf, "Hello world (GPIO%02d output 0)\r\n", led);
+    //gnss_uart_putline(0, (U08*) buf, len);
     val = HIGH;
   }
 
